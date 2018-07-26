9dddce2cf91df3d5710e680e5348f6c7a8f68a61
duxiaoyong@yeah.net <duxiaoyong@yeah.net>
2018-07-26 16:56:04 +0800

    屏蔽默认$notify方法

--- build/bin/build-entry.js
+++ build/bin/build-entry.js
@@ -39,7 +39,7 @@ const install = function(Vue, opts = {}) {
   Vue.prototype.$alert = MessageBox.alert;
   Vue.prototype.$confirm = MessageBox.confirm;
   Vue.prototype.$prompt = MessageBox.prompt;
-  Vue.prototype.$notify = Notification;
+  // Vue.prototype.$notify = Notification;
   Vue.prototype.$message = Message;
 
 };
